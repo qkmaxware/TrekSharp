60831558fcaba78e999c27d67595c9050a88d6e8
Colin Halseth <qkmaxware@gmail.com>
2021-02-28 10:45:56 -0800

    Registry information generator

--- TrekSharp/src/RandomizerExtentions.cs
+++ TrekSharp/src/RandomizerExtentions.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 
 namespace TrekSharp{
     
-public static class ListExtentions {
+public static class RandomizerExtentions {
     private static Random random = new Random();
     public static T Random<T>(this List<T> items) {
         return items[random.Next(items.Count)];   
+    }
+
+    public static int Random(this Range range) {
+        return random.Next(range.Start.Value, range.End.Value);
     }
 }
 
