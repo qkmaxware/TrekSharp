fce923bb9e7d3c025cd3203b076b4450f1b534d0
Colin Halseth <qkmaxware@gmail.com>
2021-03-08 16:45:06 -0800

    Switched absolute to relative urls

--- TrekSharp.AdventureTools/Data/UrlExtentions.cs
+++ TrekSharp.AdventureTools/Data/UrlExtentions.cs
@@ -6,12 +6,12 @@ namespace TrekSharp.AdventureTools {
 public static class UrlExtentions {
     public static string GetShipImageUrl(this Vessel Ship) {
         var name = Ship.Frame.Name.Replace(" ", string.Empty);
-        return $"/assets/artwork/ships/{name}.png";
+        return $"assets/artwork/ships/{name}.png";
     }
 
     public static string GetSpeciesImageUrl(this Species Species) {
         var name = Species.Name.Replace(" ", string.Empty);
-        return $"/assets/artwork/species/{name}.png";
+        return $"assets/artwork/species/{name}.png";
     }
 }
 
