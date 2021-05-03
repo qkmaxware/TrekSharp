38f5157bfe3ed3b816cf6f71dd9ce0eaeb4d3055
Colin Halseth <qkmaxware@gmail.com>
2021-05-03 14:39:25 -0700

    Multiple logs for the same date are allowed; promotion editor; minor layout changes

--- TrekSharp.AdventureTools/Data/Log.cs
+++ TrekSharp.AdventureTools/Data/Log.cs
@@ -6,22 +6,44 @@ using System.Linq;
 namespace TrekSharp.AdventureTools {
 
 public class Logbook : IEnumerable<LogEntry> {
-    public SortedList<double, LogEntry> Entries {get; set;}
-    public LogEntry Earliest => Entries?.Min().Value;
-    public LogEntry Latest => Entries?.Max().Value;
+    public SortedList<double, List<LogEntry>> Entries {get; set;}
+    public LogEntry Earliest => Entries?.Min().Value.FirstOrDefault();
+    public LogEntry Latest {
+        get {
+            var last_set = Entries?.Max().Value;
+            if (last_set.Count < 1)
+                return null;
+            var last_value = last_set[last_set.Count - 1];
+            return last_value;
+        }
+    }
     public int Count => Entries?.Count ?? 0;
     public Logbook() {
-        this.Entries = new SortedList<double, LogEntry>();
+        this.Entries = new SortedList<double, List<LogEntry>>();
     }
     public void Record(LogEntry entry) {
-        this.Entries.Add(entry.Stardate, entry);
+        if (Entries.ContainsKey(entry.Stardate)) {
+            this.Entries[entry.Stardate].Add(entry);
+        } else {
+            this.Entries.Add(entry.Stardate, new List<LogEntry> { entry });
+        }
     }
     public void Delete(LogEntry entry) {
-        this.Entries.Remove(entry.Stardate);
+        if (this.Entries.ContainsKey(entry.Stardate)) {
+            var list = this.Entries[entry.Stardate];
+            list.Remove(entry);
+            if (list.Count < 1) {
+                this.Entries.Remove(entry.Stardate);
+            }
+        }
     }
 
     public IEnumerator<LogEntry> GetEnumerator() {
-        return this.Entries?.Values?.GetEnumerator();
+        foreach (var log_set in this.Entries) {
+            foreach (var entry in log_set.Value) {
+                yield return entry;
+            }
+        }
     }
 
     IEnumerator IEnumerable.GetEnumerator() {
