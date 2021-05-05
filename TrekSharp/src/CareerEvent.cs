c7136cc8a72a71eb64dc0d5d59c3d5a51564d51b
Colin Halseth <qkmaxware@gmail.com>
2021-05-05 12:01:46 -0700

    Working on beta lifepath creator

--- TrekSharp/src/CareerEvent.cs
+++ TrekSharp/src/CareerEvent.cs
@@ -1,16 +1,14 @@
 namespace TrekSharp {
-    
-public class Environment {
+
+public class CareerEvent {
     public string Name {get; private set;}
-    public string Description {get; private set;}
     public Attributes AttributeSelectionMask {get; private set;}
     public Disciplines DisciplineSelectionMask {get; private set;}
 
-    public Environment (string name, string description, Attributes amask, Disciplines dmask) {
+    public CareerEvent (string name, Attributes amask, Disciplines dmask) {
         this.Name = name;
         this.AttributeSelectionMask = amask;
         this.DisciplineSelectionMask = dmask;
-        this.Description = description;
     }
 
     public override string ToString() {
