c41aaa98b37745fab96b0e4dc38de896f777b508
qkmaxware <qkmaxware@users.noreply.github.com>
2021-05-08 03:05:44 +0000

    Deploying to gh-pages from  @ 9e14d55622531a38f71a54809b99b3042efef01c 🚀

--- js/helpers.js
+++ js/helpers.js
@@ -78,3 +78,10 @@ async function BlazorHandleFileSelect(input) {
     return "";
   }
 }
+
+function BlazorReadLocalStorage(name) {
+  return window.localStorage.getItem(name);
+}
+function BlazorWriteLocalStorage(name, content) {
+  return window.localStorage.setItem(name, content);
+}
